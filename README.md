f9a4b01521583d9e7dca979c8f4f7e171701d1be
BrunaCelestino <brunaccelestino@gmail.com>
2022-07-16 14:08:59 -0300

    Tech Woman Summit Avanade - Node.js

---
+++ README.md
@@ -0,0 +1,6 @@
+Hoje estou concluindo o desafio Dio:
+Tech Woman Summit Avanade - Node.js
+
+Utilizando #node.js para back-end e #react pro front-end, desenvolvi uma aplicação de reservas de assentos em voos onde, ao final da reserva, o usuário recebe um e-mail de confirmação, através da plataforma #sendgrid.
+
+Experiência incrível guiada pela professora Isadora Garcia Ferrão e oferecida pela parceria entre Digital Innovation One Inc. e Avanade.
---
+++ api/app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+var db = mongoose.connection;
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use('/availability', require("./routes/availabilityRoute"));
+app.use('/reserve', require("./routes/reservationRoute"));
+
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', _ => {
+  console.log('Conectado ao banco');
+});
+
+const sendMail = async(msg) => {
+  try{
+await sgMail.send(msg);
+console.log('Mensagem enviada com sucesso!');
+  }catch(error){
+console.error(error.response.body);
+if(error.response){
+  console.error(error.response.body);
+}
+  }
+}
+
+sendMail({
+  to: "brunaccelestino@gmail.com",
+  from: "brunaccelestino@gmail.com",
+  subject: "Confirmação de cadastro do sistema",
+  text: "Parabéns, você acaba de se registrar no sistema Avanade Flights!"
+})
+
+module.exports = app;
